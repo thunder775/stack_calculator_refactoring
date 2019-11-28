c199d4f31a40100935914eb63372db216e906a50
thunder775 <rahulmasih61@gmail.com>
2019-11-28 13:59:20 +0530

    done

--- bin/main.dart
+++ bin/main.dart
@@ -16,6 +16,8 @@
 //  Any other instruction (for example, a letter) should result in the value
 //  "Invalid instruction: [instruction]"
 
+import 'package:test/test.dart';
+
 /// Examples
 //  stackCalc("") ➞ 0
 //  stackCalc("5 6 +") ➞ 11
@@ -28,46 +30,50 @@ int stackCalc(String argument) {
 
   for (int i = 0; i < walkthrough.length; i++) {
     if (walkthrough[i] == '+') {
-      if (stack.length >= 2) {
-        stack.add(stack.last + stack[stack.length - 2]);
-        stack.removeAt(stack.length - 2);
-        stack.removeAt(stack.length - 2);
-      }
+      operation(stack, add);
     } else if (walkthrough[i] == 'DUP') {
-      if (stack.isNotEmpty) {
+      if (stack.isEmpty) {
+        throw ArgumentError;
+      } else {
         stack.add(stack.last);
       }
     } else if (walkthrough[i] == 'POP') {
-      if (stack.isNotEmpty) {
+      if (stack.isEmpty) {
+        throw ArgumentError;
+      } else {
         stack.removeLast();
       }
     } else if (walkthrough[i] == '-') {
-      if (stack.length >= 2) {
-        stack.add(stack.last - stack[stack.length - 2]);
-        stack.removeAt(stack.length - 2);
-        stack.removeAt(stack.length - 2);
-      }
+      operation(stack, subtract);
     } else if (walkthrough[i] == '*') {
-      if (stack.length >= 2) {
-        stack.add(stack.last * stack[stack.length - 2]);
-        stack.removeAt(stack.length - 2);
-        stack.removeAt(stack.length - 2);
-      }
+      operation(stack, multiply);
     } else if (walkthrough[i] == '/') {
-      if (stack.length >= 2) {
-        stack.add(stack.last ~/ stack[stack.length - 2]);
-        stack.removeAt(stack.length - 2);
-        stack.removeAt(stack.length - 2);
-      }
+      operation(stack, divide);
     } else {
       (stack.add(int.parse(walkthrough[i])));
     }
   }
-
-
+//print(stack);
   return stack.last;
 }
 
+void operation(List list, Function func) {
+  if (list.length < 2) {
+    throw ArgumentError;
+  }
+  int arg1 = list.removeLast();
+  int arg2 = list.removeLast();
+  list.add(func(arg1, arg2));
+}
+
+int add(int a, int b) => (a + b);
+
+int subtract(int a, int b) => (a - b);
+
+int multiply(int a, int b) => (a * b);
+
+int divide(int a, int b) => (a ~/ b);
+
 main() {
-  print(stackCalc("3 *"));
+  print(stackCalc("3 +"));
 }
--- test/Stack_Calculator_Refactoring_test.dart
+++ test/Stack_Calculator_Refactoring_test.dart
@@ -1,8 +1,19 @@
 import 'package:Stack_Calculator_Refactoring/Stack_Calculator_Refactoring.dart';
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
   test('calculate', () {
     expect(calculate(), 42);
   });
+  expect(stackCalc(""),0);
+  expect(stackCalc("5 6 +"),11);
+  expect(stackCalc("3 DUP +"), 6);
+  expect(stackCalc("6 5 5 7 * - /"), 5);
+  expect(stackCalc("6 5 5 7 POP - +"), 6);
+  expect(stackCalc("2 5 6 POP +"), 7);
+//  expect(stackCalc("+ 5 6 POP +"), 'Invalid Input');
+//  expect(stackCalc("6 POP +"), 'Invalid Input');
+//  expect(stackCalc("POP 6 +"), 'Invalid Input');
+
 }
